0b43f8a0a3a5f888b57f78abc35815d8a8d263ea
Hyeonwoo <sksk2425@naver.com>
2022-05-08 01:30:15 +0900

    use cheerio

--- melon-crawler.js
+++ melon-crawler.js
@@ -6,6 +6,13 @@ function melonCrawler() {
 
   axios.get(URL).then(res => {
     console.log(res.status)
+    if(res.status == 200) {
+      let crawledMusic= [];
+
+      // res.data에 있는 tag를 cheerio로 검색하여 변수에 담기
+      const $ = cheerio.load(res.data);
+      const $musicList = $('#lst50');
+    }
   })
 }
 
