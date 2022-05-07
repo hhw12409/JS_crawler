fcf38577ebf858b52460dd7674d013ed1aabfee4
Hyeonwoo <sksk2425@naver.com>
2022-05-08 02:01:49 +0900

    add annotation commnet

--- melon-crawler.js
+++ melon-crawler.js
@@ -7,6 +7,8 @@ function melonCrawler() {
   axios.get(URL).then(res => {
     console.log(res.status)
     if(res.status == 200) {
+
+      // empty array
       let crawledMusic= [];
 
       // res.data에 있는 tag를 cheerio로 검색하여 변수에 담기
