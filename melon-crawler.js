93ab6bfba9f8816330fd777ef407e84da2ba0181
Hyeonwoo <sksk2425@naver.com>
2022-05-09 01:57:40 +0900

    make function using crawling

--- melon-crawler.js
+++ melon-crawler.js
@@ -14,6 +14,10 @@ function melonCrawler() {
       // res.data에 있는 tag를 cheerio로 검색하여 변수에 담기
       const $ = cheerio.load(res.data);
       const $musicList = $('#lst50');
+
+      $musicList.each(function(i) {
+        console.log(i)
+      })
     }
   })
 }
